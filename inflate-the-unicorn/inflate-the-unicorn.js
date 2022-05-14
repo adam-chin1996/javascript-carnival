ee709566073b2d0d44675f6a55a8f085fdeb2081
Adam Chin <elite467@gmail.com>
2022-05-15 01:25:38 +1200

    add base unicorn image

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,13 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicorn = document.getElementsByClassName('inflate-an-image')
+
+unicornArray = []
+/*
+unicornBalloonPop(){
+  alert(unicorn + ' says "Thank you"')
+}*/
