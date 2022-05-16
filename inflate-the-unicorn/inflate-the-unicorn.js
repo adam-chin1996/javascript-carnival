fa92f15b90993325eb6981e1cc4a9c384f243ca0
Adam Chin <elite467@gmail.com>
2022-05-17 00:49:50 +1200

    fix unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -6,8 +6,24 @@ console.log('Inflate The Unicorn!')
 
 let unicorn = document.getElementsByClassName('inflate-an-image')
 
-unicornArray = []
-/*
-unicornBalloonPop(){
-  alert(unicorn + ' says "Thank you"')
-}*/
+for (let i = 0; i < unicorn.length; i++) {
+  unicorn[i].onclick = inflate
+}
+
+let unicornArray = ['Unicorn1', 'Unicorn2', 'Unicorn3']
+
+function inflate(e) {
+  let unicornImage = e.target
+  if (unicornImage.src.match('./images/unicorn-0.png')) {
+    unicornImage.src = './images/unicorn-1.png'
+  } else if (unicornImage.src.match('./images/unicorn-1.png')) {
+    unicornImage.src = './images/unicorn-2.png'
+  } else if (unicornImage.src.match('./images/unicorn-2.png')) {
+    unicornImage.src = './images/unicorn-3.png'
+  } else if (unicornImage.src.match('./images/unicorn-3.png')) {
+    alert(unicornImage.id + ' says Thank You!')
+  }
+}
+console.log(unicorn)
+
+console.log(unicornArray)
