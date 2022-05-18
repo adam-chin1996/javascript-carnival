9aad6ec7f070de7e4b0dfaa7c4b47c36069ae8b1
Adam Chin <elite467@gmail.com>
2022-05-19 00:35:32 +1200

    give each table cell a click

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,14 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let holes = document.getElementsByTagName('TD')
+
+for (let i = 0; i < holes.length; i++) {
+  holes[i].onclick = holesClicked
+}
+
+function holesClicked() {}
+console.log(holes)
